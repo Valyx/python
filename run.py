35ca9ebe17b84da7ad8f02b642efbce1a5f8e796
Valyx <valyx26@gmail.com>
2021-10-17 12:27:40 +0000

    Add board are for the game

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
